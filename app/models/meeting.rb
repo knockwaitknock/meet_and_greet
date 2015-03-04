c7f214e9fabc37091048f8cba17a98b46b213368
Vasiliy Mariinskiy <mariinskiy@gmail.com>
2015-03-04 12:17:07 +0300

    meeting order

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,5 @@
 class Meeting < ActiveRecord::Base
-  default_scope { order('date_from ASC') }
+  default_scope { order('date_from ASC, time_from ASC') }
   scope :published, -> () { where(published: true) }
   scope :current, -> () { where("date_from >= ?", DateTime.now.to_date) }
 end
