30ddf51e57e27ac4e24c4b86f1cec96ec395276a
Vasiliy Mariinskiy <mariinskiy@gmail.com>
2015-04-03 00:37:39 +0300

    s3 win

--- app/uploaders/member_uploader.rb
+++ app/uploaders/member_uploader.rb
@@ -1,22 +1,25 @@
 # encoding: utf-8
 
 class MemberUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MimeTypes
   include CarrierWave::MiniMagick
 
+  # include Sprockets::Helpers::RailsHelper
+  # include Sprockets::Helpers::IsolatedHelper
+
   storage :fog
+
+  include CarrierWave::MimeTypes
   process :set_content_type
 
   def store_dir
-    "storage/#{model.class.base_class.name.underscore}/#{model.id}"
-  end
-
-  def filename
-    "#{mounted_as}#{File.extname(original_filename)}" if original_filename.present?
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   version :regular do
     process :resize_to_fill => [170,170]
   end
 
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
